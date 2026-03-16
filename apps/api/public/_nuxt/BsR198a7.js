2eb14b99867e6113015855ee6fd28762ff4354f5
Rajiv Mohan <mohan.rajiv@kpn.com>
2026-03-16 12:26:17 +0100

    add public files

---
+++ apps/api/public/_nuxt/BsR198a7.js
@@ -0,0 +1,1 @@
+import"./h8Wp5jBm.js";const e=window.setInterval;export{e as s};
---
+++ apps/api/public/_nuxt/D_femGcU.js
@@ -0,0 +1,1 @@
+import{a7 as a,a8 as o,a9 as i}from"./h8Wp5jBm.js";function c(){const t=i().public.apiUrl;return{fetchHealth:()=>o(t),postSessionStats:s=>a(t,s)}}export{c as u};
